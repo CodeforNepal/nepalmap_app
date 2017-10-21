0cc2c85a143acd5a7593d4e6547dea7e3fce3b4b
crispg72 <chris.e.green@hotmail.com>
2017-10-21 23:47:23 +0100

    Removed unused imports

--- wazimap_np/disasters.py
+++ wazimap_np/disasters.py
@@ -1,7 +1,5 @@
-from collections import OrderedDict
 
 from wazimap.data.tables import get_datatable
-from wazimap.data.utils import get_stat_data
 
 
 def get_disasters_profile(geo_code, geo_level, session):
@@ -12,16 +10,15 @@ def get_disasters_profile(geo_code, geo_level, session):
         flood_damage_table = get_datatable('flood_damage')
         flood_deaths, _ = flood_damage_table.get_stat_data(
             geo_level, geo_code, percent=False)
- 
+
         disasters_data = dict(
-         is_vdc=
-         False,
-         area_has_data=
-         True,
-         flood_deaths= {
+         is_vdc=False,
+         area_has_data=True,
+         flood_deaths={
                 'name': 'Total (flood + heavy rainfall + landslide)',
-                'values':
-                    {'this': flood_deaths['total']['values']['this']}
+                'values': {
+                    'this': flood_deaths['total']['values']['this']
+                }
             },
         )
 
