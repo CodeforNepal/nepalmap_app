3c4a1e2d75d9937b79b02671c539094e2170c167
crispg72 <chris.e.green@hotmail.com>
2017-10-21 23:30:12 +0100

    Removed debug junk

--- wazimap_np/disasters.py
+++ wazimap_np/disasters.py
@@ -9,45 +9,20 @@ def get_disasters_profile(geo_code, geo_level, session):
 
     if geo_level != 'vdc':
 
-        #flood_damage_table = get_datatable('flood_damage')
-        #flood_deaths, _ = flood_damage_table.get_stat_data(
-        #    geo_level, geo_code, percent=False)
-        print("11111")
-        #flood_deaths, _ = get_stat_data(
-        #    ['total'], geo_level, geo_code, session,
-        #    table_name='flood_damage')
-
-        flood_deaths, _ = get_stat_data(
-            ['flood', 'heavy_rainfall', 'landslide'], geo_level, geo_code, session,
-            table_name='flood_damage',
-            percent=False)
-
-        print(flood_deaths)
-        print("22222")
-        #print(flood_deaths['total']['values']['this'])
-        #ordered_decade_dist_data = OrderedDict(
-        #    sorted(registered_company_decade_dist_data.items(),
-        #           key=lambda business_range: business_range[0])
-        #)
-
+        flood_damage_table = get_datatable('flood_damage')
+        flood_deaths, _ = flood_damage_table.get_stat_data(
+            geo_level, geo_code, percent=False)
+ 
         disasters_data = dict(
          is_vdc=
          False,
          area_has_data=
          True,
-         flood_deaths= 
-         flood_deaths,
-         #flood_deaths= {
-         #       'name': 'total',
-         #       'values':
-         #           {'this': flood_deaths['total']['values']['this']}
-         #   },
-
-         total_flood_deaths=
-         {
-             "name": "Companies registered since 2003 BS",
-             "values": {'this': 1234}
-         }
+         flood_deaths= {
+                'name': 'Total (flood + heavy rainfall + landslide)',
+                'values':
+                    {'this': flood_deaths['total']['values']['this']}
+            },
         )
 
     return disasters_data
