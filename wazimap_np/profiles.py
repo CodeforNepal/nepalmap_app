99578302df7e9744ffd2e51d761ee4187b3e8ba8
Nitesh Rijal <rijal.it@gmail.com>
2018-07-12 21:25:54 -0400

    clean-up PROFILE_SECTIONS without breaking the functionality of the get_census_profile method

--- wazimap_np/profiles.py
+++ wazimap_np/profiles.py
@@ -3,18 +3,19 @@ from wazimap.geo import geo_data
 
 # ensure tables are loaded
 
-PROFILE_SECTIONS = (
-    'agriculture',
-    'business',
-    'demographics',
-    'development',
-    'disasters',
-    'education',
-    'elections',
-    'forests',
-    'health',
-    'households'
-)
+# TODO: keeping this tuple because it is referenced in function code, may be needed to bring back
+# PROFILE_SECTIONS = (
+#     'agriculture',
+#     'business',
+#     'demographics',
+#     'development',
+#     'disasters',
+#     'education',
+#     'elections',
+#     'forests',
+#     'health',
+#     'households'
+# )
 
 
 def get_census_profile(geo_code, geo_level, profile_name=None):
@@ -24,17 +25,18 @@ def get_census_profile(geo_code, geo_level, profile_name=None):
         geo_summary_levels = geo_data.get_summary_geo_info(geo_code, geo_level)
         data = {}
 
-        for section in PROFILE_SECTIONS:
-            function_name = 'get_%s_profile' % section
-            if function_name in globals():
-                func = globals()[function_name]
-                data[section] = func(geo_code, geo_level, session)
-
-                # get profiles for province and/or country
-                for level, code in geo_summary_levels:
-                    # merge summary profile into current geo profile
-                    merge_dicts(data[section], func(code, level, session),
-                                level)
+        # TODO: commented out because all the other dependent functions are removed, take action if needed
+        # for section in PROFILE_SECTIONS:
+        #     function_name = 'get_%s_profile' % section
+        #     if function_name in globals():
+        #         func = globals()[function_name]
+        #         data[section] = func(geo_code, geo_level, session)
+        #
+        #         # get profiles for province and/or country
+        #         for level, code in geo_summary_levels:
+        #             # merge summary profile into current geo profile
+        #             merge_dicts(data[section], func(code, level, session),
+        #                         level)
 
     finally:
         session.close()
